eaa31cf0fc67eada138220a05571ec10a504639f
“Евгений <kea-asd@yandex.ru>
2022-03-27 00:27:52 +0700

    Add files

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+
+function openModal() {
+    document.getElementById("modal").style.left = "0px";
+}
+// function openModal() {    
+//     document.getElementById("modal-popup").style.top = "1589px";
+// }
+function closeModal() {
+    document.getElementById("modal").style.left = "-200%";
+}
