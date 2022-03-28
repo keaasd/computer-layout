e05e007dbdb4ad4464fe9c63a38ceb1f97a1d63a
“Евгений <kea-asd@yandex.ru>
2022-03-29 00:20:08 +0700

    Add hover

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-
+{
 function openModal() {
     document.getElementById("modal").style.left = "0px";
 }
@@ -8,3 +8,28 @@ function openModal() {
 function closeModal() {
     document.getElementById("modal").style.left = "-200%";
 }
+}
+// modal window
+const modalWindow = document.querySelector('.modal');
+const buttonModals = document.querySelectorAll('.modal__button');
+const modalClose = document.querySelector('.modal__close');
+const body = document.querySelector('body');
+
+buttonModals.forEach((item) => {
+	item.addEventListener('click', () => {
+		modalWindow.style.display = 'flex';
+	});
+});
+
+modalWindow.addEventListener('click', (e) => {
+	const isModal = e.target.closest('.modal__inner');
+	if (!isModal) {
+	modalWindow.style.display = 'none';
+	body.classList.remove('noscroll');
+}
+});
+
+modalClose.addEventListener('click', () => {
+	modalWindow.style.display = 'none';
+	body.classList.remove('noscroll');
+})
